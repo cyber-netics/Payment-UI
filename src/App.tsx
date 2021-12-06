ca2625e8c08b7c40a447cc12645cdc0f072ab298
Eduard Jacobs <edoter92@gmail.com>
2021-12-06 00:05:50 -0800

    auth form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import CheckoutPage from "./Pages/Checkout";
+import AuthPage from "./Pages/Auth";
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path={"checkout/*"} element={<CheckoutPage />} />
+      <Route path={"auth/*"} element={<AuthPage />} />
     </Routes>
   );
 };
